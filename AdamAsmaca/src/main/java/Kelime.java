bf05b9c14126e328ea898d7c9991284c0b8aa1dc
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-06 11:16:30 +0300

    Update Kelime.java

--- AdamAsmaca/src/main/java/Kelime.java
+++ AdamAsmaca/src/main/java/Kelime.java
@@ -1,2 +1,18 @@
+import java.util.Random;
+
 public class Kelime {
+    //POJO classes
+
+    private String[] kelimeler = {"java", "programlama", "nesne", "yazılım", "bilgisayar"};
+    private String kelime;
+
+    public Kelime() {
+        Random random = new Random();
+        kelime = kelimeler[random.nextInt(kelimeler.length)];//lengthin 1 eksigini alir
+    }
+
+    public String getKelime() {
+        return kelime;
+    }
+    
 }
