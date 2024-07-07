8a31388069c14af487ac8a498e6248e334446291
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-07 12:56:41 +0300

    Update AnaProgram.java

--- RastgeleSifreOlusturucu/src/main/java/AnaProgram.java
+++ RastgeleSifreOlusturucu/src/main/java/AnaProgram.java
@@ -5,27 +5,75 @@ public class AnaProgram {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Sifre uzunlugu giriniz : ");
-        int uzunluk = scanner.nextInt();
+        int uzunluk = 0;
+        while (true) {
+            try {
+                System.out.println("Sifre uzunlugu giriniz : ");
+                uzunluk = scanner.nextInt();
+                if (uzunluk < 0) {
+                    throw new IllegalArgumentException("Uzunluk negatif olamaz ");
+                }
+                break;//dongu kirilacak
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Rakam disinda bir karakter girdiniz ");
+                scanner.nextLine();//hata sonrasi temizlik
+            }
+        }
+        boolean buyukHarf = false;
+        while (true){
+            try {
+                System.out.println("Buyuk harf kullanilsin mi ? (true/false)");
+                buyukHarf = scanner.nextBoolean();
+                break;
+            } catch (Exception e) {
+                System.out.println("Buyuk harf disinda bir karakter girdiniz");
+                scanner.nextLine();
+            }
+        }
 
-        System.out.println("Buyuk harf kullanilsin mi ? (true/false)");
-        boolean buyukHarf = scanner.nextBoolean();
+        boolean kucukHarf = false;
+        while (true){
+            try {
+                System.out.println("Kucuk harf kullanilsin mi? (true/false)");
+                kucukHarf = scanner.nextBoolean();
+                break;
+            } catch (Exception e) {
+                System.out.println("Kucuk harf disinda bir karakter girdiniz");
+                scanner.nextLine();
+            }
+        }
 
-        System.out.println("Kucuk harf kullanilsin mi? (true/false)");
-        boolean kucukHarf = scanner.nextBoolean();
+        boolean rakam = false;
+        while (true){
+            try {
+                System.out.println("Rakam kullanilsin mi? (true/false)");
+                rakam = scanner.nextBoolean();
+                break;
+            } catch (Exception e) {
+                System.out.println("Kucuk harf disinda bir karakter girdiniz" );
+                scanner.nextLine();
+            }
+        }
 
-        System.out.println("Rakam kullanilsin mi? (true/false)");
-        boolean rakam = scanner.nextBoolean();
-
-        System.out.println("Ozel karakter kullanilsin mi? (true/false)");
-        boolean ozelKarakter = scanner.nextBoolean();
+        boolean ozelKarakter = false;
+        while (true){
+            try {
+                System.out.println("Ozel karakter kullanilsin mi? (true/false)");
+                ozelKarakter = scanner.nextBoolean();
+                break;
+            } catch (Exception e) {
+                System.out.println("Kucuk harf disinda bir karakter girdiniz" );
+                scanner.nextLine();
+            }
+        }
 
         SifreAyarlar ayarlar = new SifreAyarlar(uzunluk, buyukHarf, kucukHarf, rakam, ozelKarakter);
         SifreOlusturucu olusturucu = new SifreOlusturucu();
 
         String rastgeleSifre = olusturucu.sifreOlustur(ayarlar);
         System.out.println("Olusturulan sifre: " + rastgeleSifre);
-
 
 
     }
