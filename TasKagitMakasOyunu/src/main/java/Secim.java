e4c3f3863377d79ab595ca46064e755fcfe4203e
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-08 17:29:06 +0300

    Update Secim.java

--- TasKagitMakasOyunu/src/main/java/Secim.java
+++ TasKagitMakasOyunu/src/main/java/Secim.java
@@ -1,2 +1,23 @@
-public class Secim {
+import java.util.Random;
+
+public enum Secim {
+    TAS, KAGIT, MAKAS;
+
+    public static Secim rastgeleSecim() {
+        return values()[(int)(Math.random()*values().length)];//Bu satir secim enum'in tüm degerini bir dizi olarak dondurur ve
+                                                              // Math.random() methodu ile bir deger dondurur.
+    }
+
+    public static Secim secimDeger(String secim){
+        switch (secim.toUpperCase()){
+            case "TAS":
+                return TAS;
+            case "KAGIT":
+                return KAGIT;
+            case "MAKAS":
+                return MAKAS;
+            default:
+                return null;
+        }
+    }
 }
