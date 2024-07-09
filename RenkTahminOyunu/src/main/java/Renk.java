34e0a5b99518d827f727337f636834806a7b6318
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-09 13:16:27 +0300

    Update Renk.java

--- RenkTahminOyunu/src/main/java/Renk.java
+++ RenkTahminOyunu/src/main/java/Renk.java
@@ -1,5 +1,10 @@
 public enum Renk {
 
-    PEMBE, KIRMIZI, MAVI, SARI, BEYAZ
+    PEMBE, KIRMIZI, MAVI, SARI, BEYAZ;
+
+    public static Renk rastgeleSecim(){
+        return values()[(int) (Math.random()*values().length)];
+    }
+
 
 }
