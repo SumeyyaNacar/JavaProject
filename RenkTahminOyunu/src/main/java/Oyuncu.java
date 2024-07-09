74258bc3717ba3d27a80b050e509cfd1cb682831
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-09 13:19:32 +0300

    Update Oyuncu.java

--- RenkTahminOyunu/src/main/java/Oyuncu.java
+++ RenkTahminOyunu/src/main/java/Oyuncu.java
@@ -1,2 +1,27 @@
 public class Oyuncu {
+    private String isim;
+    private int dogruSayisi;
+
+    public Oyuncu(String isim) {
+        this.isim = isim;
+        this.dogruSayisi = 0;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public int getDogruSayisi() {
+        return dogruSayisi;
+    }
+
+    public void dogruSayisiArttir() {
+        dogruSayisi++;
+    }
+
+    public void dogruSayisiSifirla() {
+        dogruSayisi = 0;
+    }
+
+    
 }
