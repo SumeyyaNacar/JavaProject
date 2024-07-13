bac286781dfaf1b9856f681a80d9a85105d889f9
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-13 16:31:19 +0300

    Create AracSigortaApplication.java

---
+++ AracSigortaApplications/src/aracSigorta/AracSigortaApplication.java
@@ -0,0 +1,20 @@
+package aracSigorta;
+
+    /*
+    Proje: Arac Siborta Prim Hesaplama
+           Arac tipleri: otomobil, kamyon, otobus, motosiklet
+                         --otobus : 18-30 koltuk veya 31 ve ustu koltuk
+           Tarife donemi : Aralik 2022, Haziran 2022
+           1. Donem:                            2. Donem:
+              otomobil : 2000                      otomobil : 2500
+              kamyon : 3000                        kamyon : 3500
+              otobus : tip1 : 4000 tip2: 5000      otobus : tip1 : 4500 tip2: 5500
+              motosiklet : 1500                    motosiklet : 1750
+           Hatali giriste hesaplama basarisiz uyarisi verip tekrar menu gosterilsin.
+
+     */
+
+public class AracSigortaApplication {
+
+
+}
