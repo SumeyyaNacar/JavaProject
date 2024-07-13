a30fc30b1746ccaa6d116f00d7b1daefc4ab5ed2
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-13 16:08:13 +0300

    Update SayisalLoto.java

--- MiniProject/src/main/java/SayisalLoto.java
+++ MiniProject/src/main/java/SayisalLoto.java
@@ -15,12 +15,18 @@ public class SayisalLoto {
      */
 
     public static void main(String[] args) {
+
+        startLoto();
+
+    }
+
+    public static void startLoto(){
         Scanner input = new Scanner(System.in);
-        System.out.print("Sayisal loto icin hane sayisi giriniz");
+        System.out.print("Sayisal loto icin hane sayisi giriniz : ");
         int slots = input.nextInt();
-        System.out.println("Sayilar icin min deger giriniz");
+        System.out.print("Sayilar icin min deger giriniz : ");
         int min = input.nextInt();
-        System.out.println("Sayilar icin max deger giriniz");
+        System.out.print("Sayilar icin max deger giriniz : ");
         int max = input.nextInt();
 
         int[] lotArr = new int[slots];
@@ -38,25 +44,74 @@ public class SayisalLoto {
                 //randomNumber1 = random.nextInt(10+1-5)+5;--> 0,1,2,3,4,5--->5,6,7,8,9,10
                 //icine arguman olarak aldigi sayinin 0 ile girilen deger arasinda rastgele sayi uretir.
                 // ust sinir olan deger dahil degil
-                //aralil 0 'dan baslamasin diye min. degeri  ekleyerek biz alt limit belirliyoruz ancak
+                //aralik 0 'dan baslamasin diye min. degeri  ekleyerek biz alt limit belirliyoruz ancak
                 //eklenen min deger araliktaki butun elemanlara eklendigi icinde parantez icinden min
                 //degeri dusuyoruz. ust siniri max'a getirelim
 
                 randomNumber1 = random.nextInt(max + 1 - min) + min;
-                for (int j = 0; j < slots; j++) {
+                //random sayi dizide var mi kontrol et
+                for (int j = 0; j < i; j++) {
                     if (lotArr[j] == randomNumber1) {
                         isRepeated=true;//ayni sayi var diziye eklemesin ve donguyu kirip basa donsun ve yeni sayi uretsin
                         break;
                     }
                 }
-
             }while (isRepeated);
-            lotArr[i] = randomNumber1;
+            lotArr[i] = randomNumber1;//loto icin rastgele sayi
 
+            do {
+                isRepeated = false;
 
+                //randomNumber1 = random.nextInt(10+1-5)+5;--> 0,1,2,3,4,5--->5,6,7,8,9,10
+                //icine arguman olarak aldigi sayinin 0 ile girilen deger arasinda rastgele sayi uretir.
+                // ust sinir olan deger dahil degil
+                //aralik 0 'dan baslamasin diye min. degeri  ekleyerek biz alt limit belirliyoruz ancak
+                //eklenen min deger araliktaki butun elemanlara eklendigi icinde parantez icinden min
+                //degeri dusuyoruz. ust siniri max'a getirelim
+
+                randomNumber2 = random.nextInt(max + 1 - min) + min;
+                //random sayi dizide var mi kontrol et
+                for (int j = 0; j < i; j++) {
+                    if (lotArr[j] == randomNumber2) {
+                        isRepeated=true;//ayni sayi var diziye eklemesin ve donguyu kirip basa donsun ve yeni sayi uretsin
+                        break;
+                    }
+                }
+            }while (isRepeated);
+            ticketArr[i] = randomNumber2;//bilet icin rastgele sayi
+        }
+        System.out.println("Benim biletim : ");
+        for (int i = 0; i < ticketArr.length; i++) {
+            System.out.print(ticketArr[i]+ " ");
+        }
+        System.out.println();
+        System.out.println("Loto sonucu : ");
+        for (int i = 0; i < lotArr.length; i++) {
+            System.out.print(lotArr[i] +" ");
+        }
+        System.out.println();
+
+        int result =compare(lotArr,ticketArr);
+        if (result>0){
+            System.out.println("Tebrikler! " + result + " eslesme var.");
+        }else {
+            System.out.println("Maalesef eslesme yok, tekrar deneyiniz");
         }
 
 
+    }
+
+    public static int compare(int[] arr, int[] brr){
+
+        int counter = 0;//eslesmeyi sayip icine koyacagiz
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < brr.length; j++) {
+                if (arr[i]==brr[j]){
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 
 }
