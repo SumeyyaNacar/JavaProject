32a179fae210d5e331aef467c75c21534cf6cab1
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-14 18:07:29 +0300

    Create BiletRezervasyon.java

---
+++ BiletFiyatHesaplama/src/BiletRezervasyon.java
@@ -0,0 +1,21 @@
+public class BiletRezervasyon {
+/*
+Project: mesafeye ve sartlara gore ucak bileti fiyati hesaplayan uygulama
+         Kullanicidan mesafe(km), yasi ve yolculuk tipi(Tek Yon, Gidis-Donus)
+         koltuk no. bilgilerini alin.
+         Mesafe basina ucret 1 TL/km olarak alin (Gidis-Donus*2)
+         Ilk olarak ucusun toplam fiyatini hesaplayin ve sonrasindaki kosullara gore musteriye asagidaki
+         kurallari uygulayin:
+
+         Kullanicidan alinan degerler gecerli (mesafe ve yas degerleri pozitfi sayi, yolculuk tipi ise 1 ve 2)
+         olmalidir. Aksi takdirde kullaniciya "Hatali Veri Girdiniz !" şeklinde bir uyari verilmelidir.
+         1-Kisi "Yolculuk Tipi"ni gidis-donus secmisse bilet fiyati uzerinden %20 indirim uygulanir.
+         2.Yas indirimi:
+            Kisi 12 yasindan kucukse bilet fiyati uzerinden %50 indirim uygulanir.
+            Kisi 12-24 yaslari  arasinda ise bilet fiyati uzerinden %10 indirim uygulanir.
+            Kisi 65 yasindan buykse bilet fiyati uzerinden %30 indirim uygulanir.
+         3. Tekli Koltuk Ucreti:
+            Koltuk numarasi 3 veya 3 un kati ise bilet fiyati %20 artirilir.
+ */
+
+}
