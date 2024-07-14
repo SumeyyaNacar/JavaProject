1cb20f15850cfdc04e4ede6ad99553577a310160
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-14 19:04:27 +0300

    Update Bilet.java

--- BiletFiyatHesaplama/src/Bilet.java
+++ BiletFiyatHesaplama/src/Bilet.java
@@ -5,9 +5,10 @@ public class Bilet {
     public double price;
     public int seatNo;
 
-    public void printBilet() {
+    public void printBilet(Bus bus) {//obje uzerinden cagiracagiz, static yapmaya gerek yok
         System.out.println("Toplam tutar: " + price);
         System.out.println("---Bilet Detay---");
+        System.out.println("Otobus plakasi : "+ bus.numberPlate);
         System.out.println("Mesafe :" + distance);
         System.out.println("Yolculuk tipi : " + (typeNo == 1 ? "Tek Yon" : "Cift Yon"));
         System.out.println("Koltuk No : " + seatNo);
