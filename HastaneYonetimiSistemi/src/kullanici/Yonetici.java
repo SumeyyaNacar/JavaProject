f55b4c6ded32898842f050c657c0eec74268a0df
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-15 19:08:48 +0300

    Update Yonetici.java

--- HastaneYonetimiSistemi/src/kullanici/Yonetici.java
+++ HastaneYonetimiSistemi/src/kullanici/Yonetici.java
@@ -1,4 +1,7 @@
 package kullanici;
 
 public class Yonetici extends Kullanici{
+    public Yonetici(int id, String kullaniciAdi, String sifre, String rol) {
+        super(id, kullaniciAdi, sifre, rol);
+    }
 }
