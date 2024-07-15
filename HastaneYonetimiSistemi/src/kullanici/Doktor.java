b88941667cc961c1054f5d559396858be2864a65
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-15 19:11:31 +0300

    Update Doktor.java

--- HastaneYonetimiSistemi/src/kullanici/Doktor.java
+++ HastaneYonetimiSistemi/src/kullanici/Doktor.java
@@ -1,4 +1,8 @@
 package kullanici;
 
 public class Doktor extends Kullanici{
+
+    public Doktor(int id, String kullaniciAdi, String sifre, String rol) {
+        super(id, kullaniciAdi, sifre, rol);
+    }
 }
