971c6b5307e73d8f174af1c87926da0a39f43e8e
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-15 19:07:34 +0300

    Update Kullanici.java

--- HastaneYonetimiSistemi/src/kullanici/Kullanici.java
+++ HastaneYonetimiSistemi/src/kullanici/Kullanici.java
@@ -1,5 +1,48 @@
 package kullanici;
 
 public abstract class Kullanici {
-    
+
+    private int id;//kullanicinin benzersiz kimlik TC
+    private String kullaniciAdi;//kullanicinin adi
+    private String sifre;
+    private String rol; //kullanicinin rolu(doktor, hasta, yonetici)
+
+    //private degiskenler-- sadece bu class icerisinden erisilebilir
+    //encapsulation-- veri guvenligini ve veri butunlugunu saglar.
+    //public: her yerden erisilebilir
+    //protected: ayni paket icerisinden veya alt siniflardan erisilebilir
+    //default : Ayni paket icerisinden erisilebilir
+
+
+    public Kullanici(int id, String kullaniciAdi, String sifre, String rol) {
+        this.id = id;
+        this.kullaniciAdi = kullaniciAdi;
+        this.sifre = sifre;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getKullaniciAdi() {
+        return kullaniciAdi;
+    }
+
+    public void setKullaniciAdi(String kullaniciAdi) {
+        this.kullaniciAdi = kullaniciAdi;
+    }
+
+    public String getSifre() {
+        return sifre;
+    }
+
+    public void setSifre(String sifre) {
+        this.sifre = sifre;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
 }
