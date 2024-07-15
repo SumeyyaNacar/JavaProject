bc89661891af913ae80292ddf59584989db3f122
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-15 19:11:43 +0300

    Update Hasta.java

--- HastaneYonetimiSistemi/src/kullanici/Hasta.java
+++ HastaneYonetimiSistemi/src/kullanici/Hasta.java
@@ -1,4 +1,8 @@
 package kullanici;
 
 public class Hasta extends Kullanici{
+
+    public Hasta(int id, String kullaniciAdi, String sifre, String rol) {
+        super(id, kullaniciAdi, sifre, rol);
+    }
 }
