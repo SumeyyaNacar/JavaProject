a999ad3d3df3923b30e9dfb6ae31ed90bb2b441f
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-15 20:00:35 +0300

    Update Yonetici.java

--- HastaneYonetimiSistemi/src/kullanici/Yonetici.java
+++ HastaneYonetimiSistemi/src/kullanici/Yonetici.java
@@ -1,5 +1,7 @@
 package kullanici;
 
+import java.util.Scanner;
+
 public class Yonetici extends Kullanici{
     public Yonetici(int id, String kullaniciAdi, String sifre, String rol) {
         super(id, kullaniciAdi, sifre, rol);
@@ -8,5 +10,67 @@ public class Yonetici extends Kullanici{
     @Override
     public void menu() {
 
+        Scanner scanner= new Scanner(System.in);
+        while (true) {
+            System.out.println("*** Yonetici Menusu ***");
+            System.out.println("1. Randevu Yonetimi");
+            System.out.println("2. Kullanici Yonetimi");
+            System.out.println("3. Raporlar ve Analiz ");
+            System.out.println("4. Bildirimler ve Uyarilar");
+            System.out.println("5. Ilac Yonetimi");
+            System.out.println("6. Cikis");
+            int secim = scanner.nextInt();
+
+            switch (secim) {
+                case 1:
+                    randevuYonetimi();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    return;
+                default:
+                    System.out.println("Gecersiz Secim");
+            }
+        }
+
+    }
+
+    private void randevuYonetimi() {
+        Scanner scanner= new Scanner(System.in);
+        while (true){
+            System.out.println("*** Randevu Yonetimi ***");
+            System.out.println("1. Yeni Randevu Olusturma");
+            System.out.println("2. Randevulari Listeleme");
+            System.out.println("3. Randevu Detaylarini Goruntuleme");
+            System.out.println("4. Randevu Guncelleme");
+            System.out.println("5. Randevu Silme");
+            System.out.println("6. Cikis");
+            int secim = scanner.nextInt();
+
+            switch (secim){
+                case 1:
+                    randevuYonetimi();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    return;
+                default:
+                    System.out.println("Gecersiz Secim");
+            }
+        }
     }
 }
