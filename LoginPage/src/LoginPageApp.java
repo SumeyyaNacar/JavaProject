13f4a4dd4468971ff39fab36b440aa09509d4829
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-16 20:24:15 +0300

    Create LoginPageApp

---
+++ LoginPage/src/LoginPageApp.java
@@ -0,0 +1,37 @@
+public class LoginPageApp {
+
+/*
+Project: Bir siteye üye olma ve giriş yapma sayfası tasarlayınız.
+
+         menü: kullanıcıya işlem seçimi için menü gösterilir.
+
+         üye olma(register): kullanıcıdan ad-soyad, kullanici adi, email ve şifre bilgileri alınız.
+                             kullanici adi, email ve şifre birer listede tutulur.
+                             aynı kullanici adi veya email kabul edilmez.
+
+         giriş(login): kullanici adi/email ve şifre girilir.
+                       kullanici adi/ email bulunamazsa kayıtlı değil, üye olun uyarısı verilir.
+                       kullanici adi/email ile aynı indekste kayıtlı şifre doğrulanırsa siteye giriş yapılır.
+
+         email validation: boşluk içermemeli
+                         : @ içermeli
+                         : gmail.com,hotmail.com veya yahoo.com ile bitmeli.
+                         : mailin kullanıcı adı kısmında(@ den önce) sadece büyük-küçük harf,rakam yada -._ sembolleri olabilir.
+
+         password validation: boşluk içermemeli
+                            : en az 6 karakter olmalı
+                            : en az bir tane küçük harf içermeli
+                            : en az bir tane büyük harf içermeli
+                            : en az bir tane rakam içermeli
+                            : en az bir tane sembol içermeli
+*/
+    public static void main(String[] args) {
+        start();
+
+    }
+
+    private static void start() {
+
+    }
+
+}
