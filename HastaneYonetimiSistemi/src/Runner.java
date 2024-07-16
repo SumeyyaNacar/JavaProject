d4f1ab325550970fbcc5937ffe70625850f8af4b
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-16 20:02:50 +0300

    Update Runner.java

--- HastaneYonetimiSistemi/src/Runner.java
+++ HastaneYonetimiSistemi/src/Runner.java
@@ -1,10 +1,29 @@
 import kullanici.Doktor;
 import kullanici.Kullanici;
 import kullanici.KullaniciIslemleri;
+import kullanici.Yonetici;
+
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
         KullaniciIslemleri kullaniciIslemleri = new KullaniciIslemleri();
-        //kullaniciIslemleri.kullaniciEkle(new Doktor(1,"dr1","dr123", "ortopedi"));
+        //kullaniciIslemleri.kullaniciEkle(new Doktor(901,"Dr", "Dr1234", "Kardiyoloji");
+
+        //kullaniciIslemleri.kullaniciEkle(new Yonetici(1, "admin", "admin"));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kullanici Adinizi Giriniz");
+        String kullaniciAdi = scanner.nextLine();
+        System.out.println("Sifrenizi Giriniz");
+        String sifre = scanner.nextLine();
+        Kullanici kullanici = kullaniciIslemleri.girisYap(kullaniciAdi, sifre);
+        if (kullanici !=null){
+            System.out.println("Hosgeldiniz "+ kullanici.getKullaniciAdi());
+            kullanici.menu();
+        }else {
+            System.out.println("Gecersiz Kullanici Adi");
+        }
+
+
     }
 }
