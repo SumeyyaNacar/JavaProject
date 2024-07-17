f39908648ffcc2f916487736de533b2725baf5aa
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-17 23:57:28 +0300

    Create User.java

--- LoginPage/src/User.java
+++ LoginPage/src/User.java
@@ -1,2 +1,19 @@
 public class User {
+    public String name;
+    public String username;
+    public String email;
+    public String password;
+
+    //her class olusturulurken default olarak bir parametresiz constructor bulunur.
+    // ancak ben olusturdugum instance variable'lari set ederken onu kullanamam.
+    // yani degiskenlerime deger atayamam
+    // bu nedenle parametreli bir constructor'a ihtiyacim var
+
+    public User(String name, String username, String email, String password) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
 }
