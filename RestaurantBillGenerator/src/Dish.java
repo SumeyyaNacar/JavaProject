fa4c80d1091ba1e91315b961fc7f49f2df1a26ba
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-19 18:50:55 +0300

    Update Dish.java

--- RestaurantBillGenerator/src/Dish.java
+++ RestaurantBillGenerator/src/Dish.java
@@ -1,2 +1,46 @@
+import java.util.List;
+
 public class Dish {
+
+    private int code;
+    private String name;
+    private Double price;
+
+    public Dish(Double price, String name, int code) {
+        this.price = price;
+        this.name = name;
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {//dish classindan olusturdugum objeleri direk yazdirmak icin override etmemiz gerekir
+        return  "code=" + code +
+                ", name='" + name + '\'' +
+                ", price=" + price ;
+
+    }
 }
