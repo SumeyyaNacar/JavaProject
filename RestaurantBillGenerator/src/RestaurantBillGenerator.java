d625c636c59c3f96066eac7af09a0e15e5cdc764
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-19 18:41:38 +0300

    Update RestaurantBillGenerator.java

--- RestaurantBillGenerator/src/RestaurantBillGenerator.java
+++ RestaurantBillGenerator/src/RestaurantBillGenerator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class RestaurantBillGenerator {
    /*
    Proje: Restaurant Fis Uretme Uygulamasi(BillGenerator)
@@ -11,21 +13,57 @@ public class RestaurantBillGenerator {
 
           4- Yiyecek menu: Listedeki yiyecekler menu seklinde listelensin
              Siparis girme: Yiyecegin kodu ve istenilen adet girilerek siparis olusturulsun
-                            Her siparis ici n
+                            Her siparis icin secim menusu gosterilsin
 
-
-
-
+             Siparis iptal: Siparis kodu girilerek siparis silinsin
+             Hesap olusturma : Tutarlari ile birlikte siparisleri ve toplam tutari gosteren
+                               bir hesap fisi yazdirilsin
     */
+   public static void main(String[] args) {
+       start();
+
+   }
+
+    private static void start() {
+    }
+    public static void selectionMenu(){
+        Scanner input = new Scanner(System.in);
+        int select =-1;
+        while (select!=0){
+            System.out.println("========================================");
+            System.out.println("*** Lezzet Restaurant Siparis Menusu ***");
+            System.out.println("1- Menu");
+            System.out.println("2- Siparis Girme");
+            System.out.println("3- Siparisi Iptal Etme");
+            System.out.println("4- Hesap Olusturma");
+            System.out.println("0- Cikis");
+            System.out.print("Seciminiz : ");
+            select = input.nextInt();
+            System.out.println("========================================");
+            switch (select){
+                case 1:
+                    menu();
+                    break;
+                case 2:
+                    siparisGirme();
+                    break;
+                case 3:
+                    siparisIptalEtme();
+                    break;
+                case 4:
+                    hesapOlusturma();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Hatali giris yaptiniz");
+            }
+
+        }
+        System.out.println("Iyi gunler dileriz");
 
 
-
-
-
-
-
-
-
+    }
 
 
 }
