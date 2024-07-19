3bc4dce2a471dc83d8c1aaf8ba12f355dc88af54
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-19 18:25:06 +0300

    Create RestaurantBillGenerator.java

---
+++ RestaurantBillGenerator/src/RestaurantBillGenerator.java
@@ -0,0 +1,31 @@
+public class RestaurantBillGenerator {
+   /*
+   Proje: Restaurant Fis Uretme Uygulamasi(BillGenerator)
+          1- Bir restaurantin online sisteminde hesabi yazdiran uygulama tasarlayiniz
+
+          2- Restauranttaki yiyecekler bir listte tutulsun.
+          Yiyeceklerin kodu, ismi ve ucreti olsun.
+
+          3- Yiyecek menusu, siparis olusturma/iptal ve hesap olusturma icin secim menusu
+          gosterilsin
+
+          4- Yiyecek menu: Listedeki yiyecekler menu seklinde listelensin
+             Siparis girme: Yiyecegin kodu ve istenilen adet girilerek siparis olusturulsun
+                            Her siparis ici n
+
+
+
+
+    */
+
+
+
+
+
+
+
+
+
+
+
+}
