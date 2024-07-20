2bf1b051118a5bfc4f128ce7188b1c8d26377ae1
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-20 23:49:35 +0300

    Update OrderService.java

--- RestaurantBillGenerator/src/OrderService.java
+++ RestaurantBillGenerator/src/OrderService.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class OrderService {
+
+    List<Order> orderList = new ArrayList<>();
+
+    public void createOrder(){
+
+        Scanner inp = new Scanner(System.in);
+        int dishCode=-1;
+        while (dishCode!=0){
+            System.out.print("Lutfen urun kodunu giriniz : (Cikis: O)");
+            dishCode = inp.nextInt();//girilen kod ile urunu bulmamiz gerekiyor
+
+            
+
+
+        }
+    }
+
+
+
 }
