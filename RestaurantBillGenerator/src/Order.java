6f2e1f5c4489b7512cc7c8613f822a36de0315c1
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-20 23:43:02 +0300

    Update Order.java

--- RestaurantBillGenerator/src/Order.java
+++ RestaurantBillGenerator/src/Order.java
@@ -1,2 +1,22 @@
 public class Order {
+    public int orderCode;
+    public Dish dish;
+    public int numberOfDish;
+    public Double orderPrice;
+
+
+    public Order(Dish dish, int numberOfDish) {
+        this.dish = dish;
+        this.numberOfDish = numberOfDish;
+    }
+    public void setOrderCode(int orderCode){
+        this.orderCode = orderCode;
+    }
+    public void setPrice(){
+        this.orderPrice = this.dish.getPrice()*this.numberOfDish;
+    }
+
+    
+
+
 }
