194ed4993489a19119daa696ff2387f3826ee69b
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-22 14:58:09 +0300

    Update VeriTabani.java

--- sqlm/src/main/java/VeriTabani.java
+++ sqlm/src/main/java/VeriTabani.java
@@ -3,14 +3,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class VeriTabani {
-    private final String URL="jdbc:postgresql://localhost:5432/practice";
-    private final String USER = "admin";
-    private final String PASSWORD ="Admin123";
+    private final String URL="jdbc:postgresql://localhost:5432/B254JDBC_nt_tekrar";
+    private final String USER = "B254nt_tekrar";
+    private final String PASSWORD ="password";
 
     public Connection baglantiyiAc(){
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
         }catch (SQLException e) {
+             e.printStackTrace();
               return null;
         }
     }
@@ -22,6 +23,7 @@ public class VeriTabani {
                 con.close();
             }
         }catch (SQLException e){
+            e.printStackTrace();
             System.out.println("Veri tabani kapatilamadi!");
         }
    }
