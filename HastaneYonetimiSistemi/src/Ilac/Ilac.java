5ae0c8f3d3ae9d115da6950f4679c518cfb950ce
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-23 23:44:21 +0300

    Update Ilac

--- HastaneYonetimiSistemi/src/Ilac/Ilac.java
+++ HastaneYonetimiSistemi/src/Ilac/Ilac.java
@@ -1,4 +1,53 @@
-package Ilac;
+package ilac;
 
 public class Ilac {
+    private int id;
+    private String isim;
+    private String tur;
+    private String kullanimTalimatlari;
+    private int stok;
+
+    public Ilac(int id, String isim, String tur, String kullanimTalimatlari, int stok) {
+        this.id = id;
+        this.isim = isim;
+        this.tur = tur;
+        this.kullanimTalimatlari = kullanimTalimatlari;
+        this.stok = stok;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public String getTur() {
+        return tur;
+    }
+
+    public String getKullanimTalimatlari() {
+        return kullanimTalimatlari;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public void setTur(String tur) {
+        this.tur = tur;
+    }
+
+    public void setKullanimTalimatlari(String kullanimTalimatlari) {
+        this.kullanimTalimatlari = kullanimTalimatlari;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
 }
