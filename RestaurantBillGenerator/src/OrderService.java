c3e41f44c2200c84e70005cf3ec10d06d48295c6
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-25 23:47:23 +0300

    Update OrderService.java

--- RestaurantBillGenerator/src/OrderService.java
+++ RestaurantBillGenerator/src/OrderService.java
@@ -13,19 +13,21 @@ public class OrderService {
         while (dishCode != 0) {
             System.out.println("Lutfen urun kodunu giriniz : (Cikis: O)");//urun kodu
             dishCode = inp.nextInt();//girilen kod ile urunu bulmamiz gerekiyor
-            Dish dish = dishService.findDishCode(dishCode);//girilen kod ile yemegi buldu
-            if (dishCode > 0) {
-                System.out.print("Adet giriniz : ");//adet
-                int num = inp.nextInt();
-                Order order = new Order(dish, num);
-                order.setPrice();//fiyati hesapladik
-                order.setOrderCode(1000 + this.orderList.size());//siparis kodu olusturuldu ve her siparis sonrasi 1 tane artiracak
-                this.orderList.add(order);//listeye siparisi ekledik
+            if (dishService.findDishCode(dishCode) != null) {
+                Dish dish = dishService.findDishCode(dishCode);//girilen kod ile yemegi buldu
+                if (dishCode > 0) {
+                    System.out.print("Adet giriniz : ");//adet
+                    int num = inp.nextInt();
+                    Order order = new Order(dish, num);
+                    order.setPrice();//fiyati hesapladik
+                    order.setOrderCode(1000 + this.orderList.size());//siparis kodu olusturuldu ve her siparis sonrasi 1 tane artiracak
+                    this.orderList.add(order);//listeye siparisi ekledik
 
-            }
-            for (Order order : this.orderList) {
-                System.out.printf("Siparis kodu:%-5s  lezzet kodu:%-4s adi:%-20s adet:%-3s\n",
-                        order.orderCode, order.dish.getCode(), order.dish.getName(), order.numberOfDish);
+                }
+                for (Order order : this.orderList) {
+                    System.out.printf("Siparis kodu:%-5s  lezzet kodu:%-4s adi:%-20s adet:%-3s\n",
+                            order.orderCode, order.dish.getCode(), order.dish.getName(), order.numberOfDish);
+                }
             }
         }
     }
