b5ae95b497368467c1927c4de4d36c09e4a2eba2
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-29 23:42:58 +0300

    Update Bildirim.java

--- HastaneYonetimiSistemi/src/bildirim/Bildirim.java
+++ HastaneYonetimiSistemi/src/bildirim/Bildirim.java
@@ -1,4 +1,21 @@
 package bildirim;
 
 public class Bildirim {
+    private int bildirimId;
+    private int kullaniciId;
+    private String mesaj;
+
+    public Bildirim(int bildirimId, int kullaniciId, String mesaj) {
+        this.bildirimId = bildirimId;
+        this.kullaniciId = kullaniciId;
+        this.mesaj = mesaj;
+    }
+
+    // Getter ve Setter metotları
+    public int getBildirimId() { return bildirimId; }
+    public void setBildirimId(int bildirimId) { this.bildirimId = bildirimId; }
+    public int getKullaniciId() { return kullaniciId; }
+    public void setKullaniciId(int kullaniciId) { this.kullaniciId = kullaniciId; }
+    public String getMesaj() { return mesaj; }
+    public void setMesaj(String mesaj) { this.mesaj = mesaj; }
 }
