f328993115d45bfd3e154d42a9088c4719d104ee
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-29 23:47:46 +0300

    Update Rapor.java

--- HastaneYonetimiSistemi/src/rapor/Rapor.java
+++ HastaneYonetimiSistemi/src/rapor/Rapor.java
@@ -1,4 +1,17 @@
 package rapor;
 
 public class Rapor {
+    private String raporTuru;
+    private String icerik;
+
+    public Rapor(String raporTuru, String icerik) {
+        this.raporTuru = raporTuru;
+        this.icerik = icerik;
+    }
+
+    // Getter ve Setter metotları
+    public String getRaporTuru() { return raporTuru; }
+    public void setRaporTuru(String raporTuru) { this.raporTuru = raporTuru; }
+    public String getIcerik() { return icerik; }
+    public void setIcerik(String icerik) { this.icerik = icerik; }
 }
