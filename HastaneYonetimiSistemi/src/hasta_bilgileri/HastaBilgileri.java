148bdfe4038aadf5d96298e2f198540633091072
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-30 23:47:52 +0300

    Update HastaBilgileri.java

--- HastaneYonetimiSistemi/src/hasta_bilgileri/HastaBilgileri.java
+++ HastaneYonetimiSistemi/src/hasta_bilgileri/HastaBilgileri.java
@@ -1,4 +1,25 @@
 package hasta_bilgileri;
 
 public class HastaBilgileri {
+    private int hastaId;
+    private String hastaAdi;
+    private int yas;
+
+    public HastaBilgileri(int hastaId, String hastaAdi, int yas) {
+        this.hastaId = hastaId;
+        this.hastaAdi = hastaAdi;
+        this.yas = yas;
+    }
+
+    public int getHastaId() {
+        return hastaId;
+    }
+
+    public String getHastaAdi() {
+        return hastaAdi;
+    }
+
+    public int getYas() {
+        return yas;
+    }
 }
